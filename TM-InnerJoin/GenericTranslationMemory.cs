054103e687315605c926ea39ec027d088f71e952
Patrick Porter <patrickport@gmail.com>
2015-06-09 20:21:10 -0400

    Added GenericTranslationProvider

---
+++ TM-InnerJoin/GenericTranslationMemory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sdl.LanguagePlatform.TranslationMemoryApi;
+using Sdl.LanguagePlatform.TranslationMemory;
+using Sdl.LanguagePlatform.Core;
+using Sdl.Core.Globalization;
+
+
+namespace TM_InnerJoin
+{
+    class GenericTranslationMemory
+    {
+        public SearchResults SourceSearch(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SearchResults TargetConcordanceSearch(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
